7f59c88bd2e749b351437cb882ad61c9c7eacc8f
Thang-Nguyen-Minh <136478740+Thang-Nguyen-Minh@users.noreply.github.com>
2025-11-16 08:08:38 +0700

    First day

---
+++ src/IntroductoryProblems/WeirdAlgorithm.java
@@ -0,0 +1,20 @@
+package IntroductoryProblems;
+
+import java.util.Scanner;
+public class WeirdAlgorithm {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		long n = sc.nextInt();
+		while(n!=1){
+			if(n%2==0){
+				System.out.print(n+" ");
+				n/=2;
+			}
+			else{
+				System.out.print(n+" ");
+				n=n*3+1;
+			}
+		}
+		System.out.println(n);
+	}
+}
